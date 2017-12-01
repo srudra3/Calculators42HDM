58ae42329911ef79ac03b39bccfbd52c3a5d26f9
Olivier Bondu <olivier.bondu@gmail.com>
2017-12-01 15:13:11 +0100

    minor changes to defaults

--- example/test.py
+++ example/test.py
@@ -8,8 +8,8 @@ mode = 'H'
 sqrts = 13000
 type = 2
 mh = 125
-mH = 500
-mA = 300
+mH = 800
+mA = 700
 tb = 1.5
 cba = 0.01
 sba = math.sqrt(1 - pow(cba, 2))
@@ -18,7 +18,8 @@ m12 = math.sqrt(pow(mhc, 2) * tb / (1 + pow(tb, 2)))
 outputFile = "out.dat"
 
 
-test = Calc2HDM(mode = 'H', sqrts = sqrts, type = type, tb = tb, m12 = m12, mh = mh, mH = mH, mA = mA, mhc = mhc, sba = sba, outputFile = outputFile)
+test = Calc2HDM(mode = 'H', sqrts = sqrts, type = type, tb = tb, m12 = m12, mh = mh, mH = mH, mA = mA, mhc = mhc, sba = sba, outputFile = outputFile, muR = 1., muF = 1.)
+test.setpdf('NNPDF30_lo_as_0130_nf_4')
 test.computeBR()
 
 
@@ -34,8 +35,8 @@ print "lambda_5 ", float(test.lambda_5)
 print "lambda_6 ", float(test.lambda_6)
 print "lambda_7 ", float(test.lambda_7)
 
-#xsec = test.getXsecFromSusHi()
-#print "xsec : ", xsec
+xsec = test.getXsecFromSusHi()
+print "xsec : ", xsec
 
 
 #test.setmA(200)
