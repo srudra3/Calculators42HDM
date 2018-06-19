56fbec1454bf0b621bb1f81672bbba8ee437eeb9
alesaggio <saggio.alessia@gmail.com>
2018-06-19 17:39:11 +0200

    vary tb for log scale

--- example/produce_xsec_for_ZA_tanb_cosba.py
+++ example/produce_xsec_for_ZA_tanb_cosba.py
@@ -31,30 +31,30 @@ def float_to_str(x, digits=2):
     tmp = ('{' + tmp + '}').format(x)
     return tmp.replace('.', 'p')
 
-#for tb in xrange(0, 50, 1):
-#    for cba in xrange(-1, 1, 5):
-for tb in np.arange(0.01, 10, 0.05):
-    for cba in np.arange(-0.99, 0.99, 0.02):
-        ta = math.tan(math.atan(tb) - math.acos(cba))
-        if ta < 0:
-            continue
-        sba = math.sqrt(1 - pow(cba, 2))
-        mhc = max(MH, MA)
-        m12 = math.sqrt(pow(mhc, 2) * tb / (1 + pow(tb, 2)))
-        x = Calc2HDM(mode = 'H', sqrts = sqrts, type = type, tb = tb, m12 = m12, mh = mh, mH = MH, mA = MA, mhc = mhc, sba = sba, outputFile = outputFile, muR = 1., muF = 1.)
-        x.setpdf('NNPDF30_lo_as_0130_nf_4')
-        x.computeBR()
-        xsec, err_integration, err_muRm, err_muRp = x.getXsecFromSusHi()
-        print "xsec, HtoZABR, AtobbBR ", xsec, x.HtoZABR, x.AtobbBR
-        sigmaBR = xsec * x.HtoZABR * x.AtobbBR * ZtollBR 
-        print "# xsec * BR = ", sigmaBR
-        results['tb'].append(tb)
-        results['cba'].append(cba)
-        results['sigma'].append(xsec)
-        results['sigma_errIntegration'].append(err_integration)
-        results['sigma_err_muRm'].append(err_muRm)
-        results['sigma_err_muRp'].append(err_muRp)
-        results['BR'].append(x.HtoZABR * x.AtobbBR * ZtollBR)
+for tb_exponent in np.arange(-2, 3, 1):
+    for tb_coeff in np.arange(0.001, 10, 1):
+        tb = tb_coeff * pow(10, tb_exponent)
+        for cba in np.arange(-0.99, 0.99, 0.02):
+            ta = math.tan(math.atan(tb) - math.acos(cba))
+            if ta < 0:
+                continue
+            sba = math.sqrt(1 - pow(cba, 2))
+            mhc = max(MH, MA)
+            m12 = math.sqrt(pow(mhc, 2) * tb / (1 + pow(tb, 2)))
+            x = Calc2HDM(mode = 'H', sqrts = sqrts, type = type, tb = tb, m12 = m12, mh = mh, mH = MH, mA = MA, mhc = mhc, sba = sba, outputFile = outputFile, muR = 1., muF = 1.)
+            x.setpdf('NNPDF30_lo_as_0130_nf_4')
+            x.computeBR()
+            xsec, err_integration, err_muRm, err_muRp = x.getXsecFromSusHi()
+            print "xsec, HtoZABR, AtobbBR ", xsec, x.HtoZABR, x.AtobbBR
+            sigmaBR = xsec * x.HtoZABR * x.AtobbBR * ZtollBR 
+            print "# xsec * BR = ", sigmaBR
+            results['tb'].append(tb)
+            results['cba'].append(cba)
+            results['sigma'].append(xsec)
+            results['sigma_errIntegration'].append(err_integration)
+            results['sigma_err_muRm'].append(err_muRm)
+            results['sigma_err_muRp'].append(err_muRp)
+            results['BR'].append(x.HtoZABR * x.AtobbBR * ZtollBR)
 
 filename = 'sigmaBR_HZA_type-2_MH-%s_MA-%s.json' % (float_to_str(MH, 1), float_to_str(MA, 2))
 with open(filename, 'w+') as f:
