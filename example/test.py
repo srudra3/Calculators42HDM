00f815d169205b58621aadcd4d90d333803aab18
kjaffel <khawla.jaffel@cern.ch>
2023-04-28 17:14:00 +0200

    fix example test after changes in Block SCALES default card

--- example/test.py
+++ example/test.py
@@ -4,6 +4,10 @@ import math
 from cp3_llbb.Calculators42HDM.Calc2HDM import *
 
 # Alex's PhD thesis parameters
+muR4ggh = 0.5
+muF4ggh = 0.5
+muR4bbh = 1.
+muF4bbh = 0.25
 mode = 'H'
 sqrts = 13000
 type = 2
@@ -18,7 +22,7 @@ m12 = math.sqrt(pow(mhc, 2) * tb / (1 + pow(tb, 2)))
 outputFile = "out.dat"
 
 
-test = Calc2HDM(mode = 'H', sqrts = sqrts, type = type, tb = tb, m12 = m12, mh = mh, mH = mH, mA = mA, mhc = mhc, sba = sba, outputFile = outputFile, muR = 1., muF = 1.)
+test = Calc2HDM(mode = 'H', sqrts = sqrts, type = type, tb = tb, m12 = m12, mh = mh, mH = mH, mA = mA, mhc = mhc, sba = sba, outputFile = outputFile, muR4ggh=muR4ggh, muF4ggh=muF4ggh, muR4bbh=muR4bbh, muF4bbh=muF4bbh)
 test.setpdf('NNPDF30_lo_as_0130_nf_4')
 test.computeBR()
 
